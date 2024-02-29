4b876b185d8bb22f927611bb2fe94135c06118f7
it_is_not_tytotka <ruibtsiov@gmail.com>
2024-02-29 15:59:35 +0300

    work with models, add user and their teams, get users that in the first team

--- algoritms.py
+++ algoritms.py
@@ -1,7 +1,5 @@
 import datetime
-
 from sqlalchemy import (
-    Row,
     ForeignKey,
     Date,
     String,
@@ -10,35 +8,28 @@ from sqlalchemy import (
     Column,
     MetaData,
     create_engine,
-    text, select,
+    select,
 )  # import engine to create connections with db
-import psycopg2
-from sqlalchemy.orm import close_all_sessions, registry, as_declarative, mapped_column, Mapped, Session, declared_attr
-from sqlalchemy_utils import database_exists, create_database, drop_database
+from sqlalchemy.orm import as_declarative, mapped_column, Mapped, Session, declared_attr
 
 url = "postgresql+psycopg://postgres:Konorra9@127.0.0.1:5432/northwind"
 engine = create_engine(url, echo=True)  # ready to connect
-# with engine.connect() as connection:   # YEEEAAH we connected
-#     cursor = connection.execute(text('SELECT * FROM customers'))
-#     for customer in cursor.scalars().all():
-#         print(customer)
-
-metadata = MetaData()   # one metadata = one dataase
+metadata = MetaData()  # one metadata = one dataase
 
 
 user_table = Table(
-    'users',
+    "users",
     metadata,
-    Column('id', Integer, primary_key=True),
-    Column('user_name', String),
-    Column('date_registration', Date),
-    Column('team_id', Integer, ForeignKey('teams.id'))
+    Column("id", Integer, primary_key=True),
+    Column("user_name", String),
+    Column("date_registration", Date),
+    Column("team_id", Integer, ForeignKey("teams.id")),
 )
 team_table = Table(
-    'teams',
+    "teams",
     metadata,
-    Column('id', Integer, primary_key=True),
-    Column('team_name', String)
+    Column("id", Integer, primary_key=True),
+    Column("team_name", String),
 )
 metadata.create_all(engine)
 
@@ -46,10 +37,12 @@ metadata.create_all(engine)
 @as_declarative()
 class AbstractModel:
     id: Mapped[int] = mapped_column(autoincrement=True, primary_key=True)
+
     @classmethod
     @declared_attr
     def __tablename__(cls):
         return cls.__name__.lower()
+
 
 class UserModel(AbstractModel):
     __tablename__ = "users"
@@ -63,10 +56,12 @@ class TeamModel(AbstractModel):
     team_name: Mapped[str] = mapped_column(String)
 
 
-team = TeamModel(team_name='B')
-user = UserModel(id = 7, user_name='Trick', team_id=1, date_registration=datetime.date.today())
+team = TeamModel(team_name="B")
+user = UserModel(
+    id=7, user_name="Trick", team_id=1, date_registration=datetime.date.today()
+)
 
-with (Session(engine) as session):
+with Session(engine) as session:
     with session.begin():
         AbstractModel.metadata.create_all(engine)
     with session.begin():
